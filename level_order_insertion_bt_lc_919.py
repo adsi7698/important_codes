7c27d55a7e8ca42212701091f208b2d647974c8f
adsi7698 <adsi7698@gmail.com>
2024-06-14 05:36:56 -0700

    lc 919 - should have added complexity in init for better efficiency

---
+++ level_order_insertion_bt_lc_919.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class CBTInserter:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+
+    def insert(self, val: int) -> int:
+        queue = []
+        queue.append(self.root)
+        new_node = TreeNode(val)
+
+        while queue:
+            cur = queue.pop(0)
+            if cur.left:
+                queue.append(cur.left)
+            else:
+                cur.left = new_node
+                break
+            if cur.right:
+                queue.append(cur.right)
+            else:
+                cur.right = new_node
+                break
+        return cur.val
+
+    def get_root(self) -> Optional[TreeNode]:
+        return self.root
+
+
+# Your CBTInserter object will be instantiated and called as such:
+# obj = CBTInserter(root)
+# param_1 = obj.insert(val)
+# param_2 = obj.get_root()
